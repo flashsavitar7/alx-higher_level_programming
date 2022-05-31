0aefd0233fe256707b72e82d4a4ff9724198ea2b
Triumph Edet <triumphedet5@gmail.com>
2022-05-31 13:43:29 -0700

    scripts

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(0,100,+1):
+for i in range(0, 101):
     if i % 15 == 0:
         print("FizzBuzz ", end= "")
     elif i % 3 == 0:
@@ -8,3 +8,4 @@ for i in range(0,100,+1):
         print("Buzz ", end = "")
     else:
         print(f"{i:d} ", end = "")
+
--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,3 +3,4 @@ def islower(c):
     if ord(c) >= (ord('a')) and ord(c) <= ord('z'):
         return True
     return False
+
