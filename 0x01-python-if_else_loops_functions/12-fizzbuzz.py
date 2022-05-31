773e5ce4ab21b6ad7cacf28f84f8c80e2ece16f0
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-06-01 00:52:02 +0100

    error fixes

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-for i in range(0, 101):
+for i in range(1, 101):
     if i % 15 == 0:
-        print("FizzBuzz ", end= "")
+        print("FizzBuzz ", end = "")
     elif i % 3 == 0:
         print("Fizz ", end = "")
     elif i % 5 == 0:
         print("Buzz ", end = "")
     else:
         print(f"{i:d} ", end = "")
-
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str_data):
-    for char in str_data:
-        if ord(char) >= 65:
-            str_data += chr(ord(char) - 32)
-    return str_data
+def uppercase(str):
+    for i in str:
+        if ord(i) >= ord('a') and ord(i) <= ord('z'):
+            i = chr(ord(i) - 32)
+    print("{:s}".format(i), end="")
+    print()
