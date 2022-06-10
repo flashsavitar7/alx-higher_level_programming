39978fdcfd8827f2ef581d82e9227756833ed5aa
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-06-10 11:54:18 +0100

    more data structs

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
+#!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-    return (a.symmetric_difference(b))
+    return (set_1.symmetric_difference(set_2))
