2f16c3803511e85676af04d1e73cb7a37cdc2165
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-06-10 01:08:45 +0100

    more data structs

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+def roman_to_int(roman_string):
+    d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+    front = 0
+    back = 0
+    ans = 0
+    length = len(roman_string)
+    
+    if roman_string == None:
+        return 0
+    else:
+        for i in range(length):
+            front = d[roman_string[i]]
+            if front > back:
+                ans = ans + front - 2 * back
+            else:
+                ans += front
+            back = front
+    return int(ans)
+
+
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,4 @@
+def only_diff_elements(set_1, set_2):
+    a = set(set_1)
+    b = set(set_2)
+    return (a.symmetric_difference(b))
