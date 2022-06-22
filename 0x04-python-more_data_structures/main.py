c1493783058a280b5f49cc7978d4c400bf85a1d7
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-06-22 01:47:01 +0100

    exceptions/objects

--- 0x04-python-more_data_structures/main.py
+++ 0x04-python-more_data_structures/main.py
@@ -1,7 +1,5 @@
-#!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
+class Person:
+    pass  # An empty block
 
-my_list = [1, 2, 3, 4, 6]
-new_list = multiply_list_map(my_list, 4)
-print(new_list)
-print(my_list)
+p = Person()
+print(p)
