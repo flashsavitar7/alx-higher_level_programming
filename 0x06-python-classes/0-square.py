ea4d24126d7d1549d4e9271542a9809328113a26
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-06-22 02:03:46 +0100

    exceptions/objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-class Square:
-    """Class Square that defines a square.
-    """
-    pass
+Square = __import__('0-square').Square
+
+my_square = Square()
+print(type(my_square))
+print(my_square.__dict__)
