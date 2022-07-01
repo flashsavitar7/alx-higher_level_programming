3fe43409a07c6a548c8e2779881425fa6a760119
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-07-02 00:16:55 +0100

    test driven python

--- 0x08-python-more_classes/main.py
+++ 0x08-python-more_classes/main.py
@@ -1,6 +1,30 @@
 #!/usr/bin/python3
-Rectangle = __import__('0-rectangle').Rectangle
+class Item:
+    rate = 0.9
+    def __init__ (self, name: str, price: float, quantity):
+        #run validations
+        
+        assert price >= 0, f"price {price} is less than 0"
+        assert quantity >= 0, f"quantity {quantity} is less than 0"
+        
+        #assign to self object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        
+    def calculate_total(self):
+        return self.price * self.quantity
+    
+    def apply_rate (self):
+        self.price = self.price * self.rate
+        
+item1 = Item("cars", 100, 3)
 
-my_rectangle = Rectangle()
-print(type(my_rectangle))
-print(my_rectangle.__dict__)
+item2 = Item("bikes", 1000, 4)
+
+item1.apply_rate()
+print(item1.price)
+
+item2.rate = 0.7
+item2.apply_rate()
+print(item2.price)
