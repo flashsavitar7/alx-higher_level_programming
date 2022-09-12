fccf9e58495283837ccb83dec77ff6b2c401783e
flashsavitar7 <99418944+flashsavitar7@users.noreply.github.com>
2022-09-12 03:00:52 +0100

    relational_mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,9 +9,8 @@ a script that lists all states
 from the database
 '''
 if __name__ == "__main__":
-    con = MySQLdb.connect(
-        host="localhost", port=3306, user=argv[1],
-        password=argv[2], database=argv[3], charset="utf8")
+    con = MySQLdb.connect(port=3306, user=argv[1],
+        password=argv[2], database=argv[3],)
     cur = db.cursor()
     cur.execute("SELECT * FROM states;")
     states = cur.fetchall()
